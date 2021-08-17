5f1cbc0a1c7dcf95502165b50302be32518d1988
Avonavi23 <anna230997@mail.ru>
2021-08-17 13:42:59 +0400

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(Math, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  let average = array.length ? sum / array.length : 0;
+  return average;
 }
